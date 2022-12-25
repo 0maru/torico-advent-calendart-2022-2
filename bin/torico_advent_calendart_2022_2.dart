8f95b65f526fe9a80ff3456e9739721d013222a3
0maru <0maru.dev@gmail.com>
2022-12-25 20:05:29 +0900

    fmt

--- bin/torico_advent_calendart_2022_2.dart
+++ bin/torico_advent_calendart_2022_2.dart
@@ -2,9 +2,20 @@ import 'package:args/args.dart';
 
 void main(List<String> arguments) {
   final parser = ArgParser();
-  parser.addFlag('help', abbr: 'h', help: 'Show usage information');
-  parser.addFlag('version', abbr: 'v', help: 'Show version number');
-  parser.addOption('target', abbr: 't');
+  parser.addFlag(
+    'help',
+    abbr: 'h',
+    help: 'Show usage information',
+  );
+  parser.addFlag(
+    'version',
+    abbr: 'v',
+    help: 'Show version number',
+  );
+  parser.addOption(
+    'target',
+    abbr: 't',
+  );
   final result = parser.parse(arguments);
   print('help: ${result['help']}');
   print('version: ${result['version']}');

f295622cfd2b0e365d712b2108038efbcba4c313
0maru <0maru.dev@gmail.com>
2022-12-25 17:14:48 +0900

    init commit

---
+++ bin/torico_advent_calendart_2022_2.dart
@@ -0,0 +1,5 @@
+import 'package:torico_advent_calendart_2022_2/torico_advent_calendart_2022_2.dart' as torico_advent_calendart_2022_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${torico_advent_calendart_2022_2.calculate()}!');
+}
---
+++ test/torico_advent_calendart_2022_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:torico_advent_calendart_2022_2/torico_advent_calendart_2022_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
